39a04ec1abdc381ba0f399cf7edc467ee7f4078f
Choi Seong Hoon <112906459+seonghoon0515@users.noreply.github.com>
2022-11-12 23:21:01 +0900

    Add files via upload

---
+++ Chapter6/FactorialTest.java
@@ -0,0 +1,13 @@
+class FactorialTest {
+	public static void main(String args[]) {
+		System.out.println(factorial(4)); // FactorialTest.factorial(4)
+	}
+
+	static long factorial(int n) {
+		long result=0;
+
+		if (n == 1) return 1;		
+
+		return n * factorial(n-1); // ¸Þ¼­µå ÀÚ½ÅÀ» È£ÃâÇÑ´Ù.
+	}
+}
