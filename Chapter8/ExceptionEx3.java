0c0a656e7f1d0300da6b5dc5151d1dc374096edc
Choi Seong Hoon <112906459+seonghoon0515@users.noreply.github.com>
2022-11-16 17:07:28 +0900

    Add files via upload

---
+++ Chapter8/ExceptionEx3.java
@@ -0,0 +1,16 @@
+class ExceptionEx3 {
+	public static void main(String args[]) {
+		int number = 100;
+		int result = 0;
+
+		for(int i=0; i < 10; i++) {
+            try {
+			result = number / (int)(Math.random() * 10);
+			System.out.println(result);
+            }
+            catch(ArithmeticException e) {
+                System.out.println("0");
+            }  // try -catch 끝
+		}  // for문의 끝
+	} // main의 끝¡
+}
