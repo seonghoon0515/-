6279524efde1de63ea5bb3fe469ef5da77ce3eed
Choi Seong Hoon <112906459+seonghoon0515@users.noreply.github.com>
2022-11-24 22:20:25 +0900

    Add files via upload

---
+++ Chapter11/MyVector2Test.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class MyVector2Test {
+	public static void main(String args[]) {
+		MyVector2 v = new MyVector2();
+		v.add("0");
+		v.add("1");
+		v.add("2");
+		v.add("3");
+		v.add("4");
+
+		System.out.println("»èÁ¦ Àü : " + v);
+		Iterator it = v.iterator();
+		it.next();
+		it.remove();
+		it.next();
+		it.remove();
+
+		System.out.println("»èÁ¦ ÈÄ : " + v);
+	}
+}
